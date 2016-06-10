6f15a32617d8d48662ae64cdcf59983acc990be4
Masato Hori <rillomas@gmail.com>
2016-06-10 20:22:01 +0900

    reduced files

--- lib/components/user_setting/user_setting.dart
+++ lib/components/user_setting/user_setting.dart
@@ -2,11 +2,12 @@
 library user_setting;
 
 import 'dart:html';
+import 'dart:async';
+import 'dart:convert' show JSON;
 import 'package:polymer/polymer.dart';
 import 'package:logging/logging.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:dartium_issue/models/models.dart';
-import 'package:dartium_issue/services/services.dart';
 
 @PolymerRegister("user-setting")
 class UserSetting extends PolymerElement {
@@ -26,7 +27,8 @@ class UserSetting extends PolymerElement {
     }
     search.defaultLanguage = defaultLanguage;
     try {
-      await _channel.updateSearchSetting(search, "");
+      Logger.root.info("updating search setting");
+      await updateSearchSetting(search, "");
       Logger.root.info("updated search setting");
     } catch (e) {
       Logger.root.warning("Failed to update search setting: ${e}");
@@ -34,9 +36,41 @@ class UserSetting extends PolymerElement {
   }
 
   void ready() {
-    var domain = AppEngineServerChannel.generateRootDomain(window.location);
-    _channel = new AppEngineServerChannel(domain);
+    var location = window.location;
+    _domain = "${location.protocol}//${location.host}/";
   }
 
-  AppEngineServerChannel _channel;
+  Future<bool> updateSearchSetting(SearchSetting setting, String mutationToken) {
+    var url = "${_domain}${USER_API_PATH}/${SEARCH_SETTING_PATH}";
+    var task = new AsyncHttpRequest<bool>();
+    var hdr = {USER_MUTATION_TOKEN_HEADER_NAME: mutationToken};
+    var data = JSON.encode(setting);
+    var f = task.request(url, (res) {
+      return true;
+    }, method: "POST", sendData: data, headers: hdr);
+    return f;
+  }
+  static const String USER_API_PATH = "api/1/user";
+  static const String SEARCH_SETTING_PATH = "searchSetting";
+  static const String USER_MUTATION_TOKEN_HEADER_NAME = "User-Mutation-Token";
+  // AppEngineServerChannel _channel;
+  String _domain;
+}
+
+/// A class to run async http requests
+class AsyncHttpRequest<Output> {
+  Future<Output> request(String url, Output process(String response), {String method: "GET", sendData , Map<String,String> headers: const{}}) async {
+    HttpRequest r;
+    try {
+      r = await HttpRequest.request(url, method:method, sendData:sendData, requestHeaders:headers);
+    } catch (error) {
+      throw error.target;
+    }
+    if (r.readyState == HttpRequest.DONE && r.status == 200) {
+      // completed normally
+      return process(r.responseText);
+    }
+    // some kind of error occured
+    throw r;
+  }
 }
--- lib/services/services.dart
+++
@@ -1,9 +0,0 @@
-library services;
-
-import 'dart:async';
-import 'dart:html';
-import 'dart:convert' show JSON;
-import 'package:dartium_issue/models/models.dart';
-
-part 'async_http_request.dart';
-part 'app_engine_server_channel.dart';
